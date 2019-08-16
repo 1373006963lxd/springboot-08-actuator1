fbb40207963b80540a9649eb20b72f9b9b451242
Your Name <you@example.com>
2019-08-16 12:31:41 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/actuator/Springboot08ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot08ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot08ActuatorApplication.class, args);
+    }
+
+}
